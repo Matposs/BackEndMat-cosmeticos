a404c97053f9abfb5d47aa3b424dc6e81bef979a
Matheus <matheusposs2012@gmail.com>
2024-07-12 16:33:54 -0300

    cors

--- server.js
+++ server.js
@@ -6,16 +6,32 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const allowedOrigins = ['https://mat-cosmeticos.vercel.app', 'http://localhost:3000'];
+
 const corsOptions = {
-    origin: 'https://mat-cosmeticos.vercel.app',
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
+
 app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request for '${req.url}'`);
+    next();
+});
+
 app.use(routes);
 
-// Middleware para capturar e logar erros
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
@@ -29,11 +45,10 @@ const startServer = async () => {
         });
     } catch (error) {
         console.error('Erro ao iniciar o servidor:', error);
-        process.exit(1); // Sai do processo com erro
+        process.exit(1);
     }
 };
 
 startServer();
 
-// Exporta a aplicação express
 module.exports = app;
