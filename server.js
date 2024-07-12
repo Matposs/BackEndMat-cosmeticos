6d9875afb6d8f6ac48662c8f505fdca68f1b5f40
Matheus <matheusposs2012@gmail.com>
2024-07-12 15:56:27 -0300

    Alterações no CORS

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(cors({
-    origin: '*'
-}));
+const corsOptions = {
+    origin: 'https://mat-cosmeticos.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
